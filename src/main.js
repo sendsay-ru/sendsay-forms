290bc4234020ff3df30c5d626c3487b2537c957f
Radiansz <lightwaveinthespace@gmail.com>
2016-04-29 11:28:28 +0300

    Added showing popup after page loading. Added loading styles from script

--- src/main.js
+++ src/main.js
@@ -6,13 +6,38 @@ import {Form} from "./classes/Form.js";
 
 	
 	var activatePopup  = function(url, options) {
-		var connector = new Connector(url);
-		var form = new Form(Popup, connector);
+		loadCss();
+		var onLoad = function() {
+			var connector = new Connector(url);
+			var form = new Form(Popup, connector);
+			document.removeEventListener('load', onLoad);
+		};
+		if (document.readyState === "complete") {
+			onLoad();
+		} else {
+			document.addEventListener('load', onLoad)
+		}
 	};
 
 	var showPopup = function(data, options) {
+		loadCss();
 		let popup = new Popup(data);
 		popup.activate(options);
+	}
+
+	var loadCss = function() {
+		var cssId = '_sendsay-styles';  // you could encode the css path itself to generate id..
+		if (!document.getElementById(cssId))
+		{
+		    var head  = document.getElementsByTagName('head')[0];
+		    var link  = document.createElement('link');
+		    link.id   = cssId;
+		    link.rel  = 'stylesheet';
+		    link.type = 'text/css';
+		    link.href = 'https://0d46bfd887bfcf061429f33315cd9c9f4c9dc35a.googledrive.com/host/0B8TfwS63_P7-RkRrWnBHRG92UzA/sendsayforms.css';
+		    link.media = 'all';
+		    head.appendChild(link);
+		}
 	} 
 	window.SENDSAY = {
 		activatePopup: activatePopup,
