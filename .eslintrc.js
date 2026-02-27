17e7d28832440f751aee1f39a3670812ebc33e85
Pavel Movchan <tz_95@mail.ru>
2026-02-27 19:00:03 +0900

    fix: fix eslint errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -63,6 +63,8 @@ module.exports = {
     'import/prefer-default-export': 'off',
     'object-curly-newline': 'off',
     'arrow-parens': [2, 'always'],
+    'template-curly-spacing': 'off',
+    indent: 'off',
   },
 
   env: {
